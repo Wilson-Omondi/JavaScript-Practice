918aa514c9e966127853d0c595246a9707399cde
wangaw@94gmail.com <wangaw94@gmail.com>
2022-05-15 19:16:49 +0300

    Practice 1: Drum-Kit

---
+++ Drum-Kit/app.js
@@ -0,0 +1,23 @@
+
+function playSound(e){
+  const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if(!audio) return; //stop function from running
+
+  key.classList.add('playing')
+  audio.currentTime = 0; //rewind to the start
+  audio.play();
+  
+};  
+
+function removeTransition(e){
+  if(e.propertyName !== 'transform') return;//skip it if not a transform
+  this.classList.remove('playing')
+} 
+
+const keys =  Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+
+
+
