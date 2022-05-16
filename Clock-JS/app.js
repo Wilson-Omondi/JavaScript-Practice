1e4cfba05ca5d32152402e2c0a7a7a7ba8cb50ad
wangaw@94gmail.com <wangaw94@gmail.com>
2022-05-16 11:25:59 +0300

    Practice 2: JS Clock

---
+++ Clock-JS/app.js
@@ -0,0 +1,34 @@
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand')
+
+
+function setDate() {
+  const now = new Date();
+
+  //getting the seconds
+  const seconds = now.getSeconds();
+  const secondDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+  //getting the minutes
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins/60) * 360) + ((seconds/60)* 6) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  //getting the hours
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+
+}
+
+//We want the function to run every second
+setInterval(setDate, 1000)
+
+setDate();
+
+
+
+
